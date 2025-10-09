1b7ee8e5a72c8e506cd1940f57fddb7f1baeab5c
Jeff Culverhouse <jeff@graystorm.com>
2025-10-09 16:04:43 -0400

    fix: tls_set call for ssl mqtt connections

--- blink_mqtt.py
+++ blink_mqtt.py
@@ -171,7 +171,7 @@ class BlinkMqtt(object):
         )
 
         if self.mqtt_config.get('tls_enabled'):
-            self.mqttcnt.tls_set(
+            self.mqttc.tls_set(
                 ca_certs=self.mqtt_config.get('tls_ca_cert'),
                 certfile=self.mqtt_config.get('tls_cert'),
                 keyfile=self.mqtt_config.get('tls_key'),
